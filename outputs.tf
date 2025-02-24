876b74acc63feb3e4c85e994b5fcc3079562872f
cortadocodes <cortado.codes@protonmail.com>
2025-02-24 12:16:46 +0000

    REF: Move bigquery resources from cluster module

--- outputs.tf
+++ outputs.tf
@@ -8,3 +8,8 @@ output "storage_bucket_url" {
   description = "The `gs://` URL of the storage bucket used to store service inputs, outputs, and diagnostics."
   value = google_storage_bucket.default.url
 }
+
+
+output "bigquery_events_table_id" {
+  value = "${google_bigquery_dataset.service_event_dataset.dataset_id}.${google_bigquery_table.service_event_table.table_id}"
+}
