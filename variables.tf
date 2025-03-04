1c6344b1fc8336cdf8719bd7ab1e22d37cda54ae
cortadocodes <cortado.codes@protonmail.com>
2025-03-04 12:57:33 +0000

    STY: Apply `terraform fmt`

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "google_cloud_project_id" {
-  type = string
+  type        = string
   description = "The ID of the google cloud project to deploy resources in."
 }
 
 
 variable "google_cloud_region" {
-  type = string
+  type        = string
   description = "The google cloud region to deploy resources in."
 }
 
 
 variable "github_account" {
-  type = string
+  type        = string
   description = "The name of the GitHub account to link to the workload identity federation provider. The provider is used to authenticate with google cloud in GitHub Actions workflows."
 }
 
 
 variable "maintainer_service_account_names" {
-  type = set(string)
+  type        = set(string)
   description = "The names of each maintainer IAM service account that should be created. They'll be prefixed with 'maintainer-'."
 }
 
