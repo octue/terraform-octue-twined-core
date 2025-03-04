517a457fb32b3f55d29984a094c5c680cf3b4d3f
cortadocodes <cortado.codes@protonmail.com>
2025-03-04 17:12:41 +0000

    DOC: Improve output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "artifact_registry_repository_name" {
   value       = google_artifact_registry_repository.service_docker_images.name
-  description = "The name of the artifact registry repository storing Octue Twined service images."
+  description = "The name of the artifact registry repository storing Twined service revision images."
 }
 
 
 output "storage_bucket_url" {
-  description = "The `gs://` URL of the storage bucket used to store service inputs, outputs, and diagnostics."
   value       = google_storage_bucket.default.url
+  description = "The `gs://` URL of the storage bucket used to store service inputs, outputs, and diagnostics."
 }
 
 
